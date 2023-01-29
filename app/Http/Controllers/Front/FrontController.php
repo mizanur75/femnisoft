7f187bda97c3705994242406c474d39ee9fc0b0a
Mizanur Rahman <mizanurrahman615@gmail.com>
2023-01-29 20:10:37 +0600

    cors problem

--- app/Http/Controllers/Front/FrontController.php
+++ app/Http/Controllers/Front/FrontController.php
@@ -63,18 +63,18 @@ class FrontController extends Controller
     }
     public function services(){
         $services = Service::orderBy('id','DESC')->take(5)->get();
-        return response()->json($services);
+        return response()->json($services)->header("Access-Control-Allow-Origin",  "*");
     }
     public function frontservices(){
         $services = Service::orderBy('id','DESC')->take(4)->get();
-        return response()->json($services);
+        return response()->json($services)->header("Access-Control-Allow-Origin",  "*");
     }
     public function blog(){
-        $blog = Blog::orderBy('id','DESC')->take(16)->get();
+        $blog = Blog::orderBy('id','DESC')->take(16)->get()->header("Access-Control-Allow-Origin",  "*");
         return response()->json($blog);
     }
     public function frontblog(){
-        $blog = Blog::orderBy('id','DESC')->take(2)->get();
+        $blog = Blog::orderBy('id','DESC')->take(2)->get()->header("Access-Control-Allow-Origin",  "*");
         return response()->json($blog);
     }
     public function faq(){
